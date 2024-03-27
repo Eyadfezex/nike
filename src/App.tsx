4d476183dd73b7779553dfdd29d76a12edc8f5ce
Eyad ahmed <Eyad932004@outloo.com>
2024-03-27 22:55:49 +0200

    edit some lines in the app setup code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import React from "react";
 import "./App.css";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections/index";
 
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">popular</section>
-      <section className="padding">superQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">specialOffers</section>
-      <section className="bg-pale-blue padding">customer reviews</section>
-      <section className="padding-x sm:py-32 px-16 w-full">Subscribe</section>
-      <section className="padding-x bg-black padding-t pb-8">footer</section>
+      <NavBar />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="py-10 padding-x">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="w-full px-16 padding-x sm:py-32">
+        <Subscribe />
+      </section>
+      <section className="pb-8 bg-black padding-x padding-t">
+        <Footer />
+      </section>
     </main>
   );
 }
