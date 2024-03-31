7a168d63a055e7f9ee915cd3189de67ae908d798
Eyad ahmed <Eyad932004@outloo.com>
2024-04-01 01:18:44 +0200

    create hero sectione

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <main className="relative">
       <NavBar />
-      <section className="xl:padding-1 wide:padding-r padding-b">
+      <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
       <section className="padding">
@@ -33,7 +33,7 @@ function App() {
       <section className="bg-pale-blue padding">
         <CustomerReviews />
       </section>
-      <section className="w-full px-16 padding-x sm:py-32">
+      <section className="w-full py-16 padding-x sm:py-32">
         <Subscribe />
       </section>
       <section className="pb-8 bg-black padding-x padding-t">
